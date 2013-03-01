3f4491ad0efba6c64be46c4a945ba767fe076623
scippio <scippio@berounet.cz>
2013-03-01 03:08:22 +0100

    vlozeny vsechny hlavicky, styly, skripty apod.

---
+++ src/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="cs" dir="ltr">
+     <head>
+          <!-- metas and title -->
+          <meta charset="utf-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <meta name="generator" content="DokuWiki">
+		<meta name="robots" content="index,follow">
+		<meta name="keywords" content="portal">
+          <meta name="description" content="Cílem Pirátské strany je na prvním místě prosazování základního práva člověka na svobodné šíření informací a striktní ochranu soukromí občana. Pirátský programProjektyČlenstvíKontakt">
+          <title>Hlavní stránka - Pirati.CZ</title>
+          <!-- social metas -->
+          <meta property="og:title" content="Hlavní stránka - Pirati.CZ">
+		<meta property="og:type" content="website">
+		<meta property="og:url" content="http://www.pirati.cz/portal">
+		<meta property="og:image" content="http://static.pirati.cz/piznam/logo.png">
+		<meta property="og:image" content="http://www.pirati.cz/_media/icons/rss_yellow.jpeg">
+		<meta property="og:image" content="https://www.pirati.cz/_media/tiskove-zpravy/logo_svobodna_hudba.png?w=380">
+		<meta property="og:image" content="http://www.pirati.cz/_media/icons/rss_yellow.jpeg">
+		<meta property="og:image" content="https://www.pirati.cz/_media/stanoviska/kopacky106.jpg?w=380">
+		<meta property="og:image" content="http://www.pirati.cz/_media/icons/rss_yellow.jpeg">
+		<meta property="og:image" content="http://www.pirati.cz/_media/static/sdilet.gif">
+		<meta property="og:image" content="http://static.pirati.cz/piznam/plachta_bez_okraje.png">
+		<meta property="og:description" content="Cílem Pirátské strany je na prvním místě prosazování základního práva člověka na svobodné šíření informací a striktní ochranu soukromí občana. Pirátský programProjektyČlenstvíKontakt">
+		<meta property="fb:admins" content="1734211895">
+          <!-- links -->
+          <link href="/favicon.png" rel="icon" type="image/png">
+          <link rel="search" type="application/opensearchdescription+xml" href="/lib/exe/opensearch.php" title="Pirati.CZ">
+		<link rel="start" href="/">
+		<link rel="contents" href="/portal?do=index" title="Index">
+          <link rel="canonical" href="https://www.pirati.cz/portal">
+          <link rel="alternate" type="application/rss+xml" title="Tiskové zprávy a stanoviska České pirátské strany" href="http://www.pirati.cz/piznam/feed.php?mode=blogtng&amp;blog=default&amp;ns=tiskove-zpravy&amp;linkto=current&amp;content=html">
+          <!-- stylesheets -->
+		<link rel="stylesheet" media="all" type="text/css" href="/css/bootstrap-responsive.min.css">
+          <link rel="stylesheet" media="all" type="text/css" href="/css/bootstrap.min.css">
+          <link rel="stylesheet" media="all" type="text/css" href="/css/all.css">
+          <link rel="stylesheet" media="all" type="text/css" href="/css/print.css">
+          <!-- javscripts -->
+          <script type="text/javascript"><!--//--><![CDATA[//><!--
+		     var NS='';var SIG=' --- //[[vaclav.malek@pirati.cz|Václav Málek (Vaclav Malek)]] 01.03.2013 02:31//';var JSINFO = {"id":"portal","namespace":""};
+		//--><!]]>
+          </script>
+          <script type="text/javascript" charset="utf-8" src="/js/jquery.min.js"></script>
+		<script type="text/javascript" charset="utf-8" src="/js/bootstrap.min.js"></script>
+          <script type="text/javascript" charset="utf-8" src="/js/raphael-min.js"></script>
+     </head>
+     <body>
+          <p>titulka</p>
+     </body>
+</html>
